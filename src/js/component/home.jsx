1b71cad9d0183d06ececbd2ee6039cb94ed53ea0
Sabrina Estévez <sesmo.dev@gmail.com>
2024-01-19 11:53:17 +0000

    termino maquetación y descarto los BONUS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,13 +6,13 @@ const Home = (props) => {
 
 	return (
 		<div className="container text-center mt-5 bg-black">
-			<div className="row">
+			<div className="row itemsContainer">
 				<div className="col reloj"><i className="fa-regular fa-hourglass-half"></i></div>
 				<div className="col numbers">
 					<div className="row">
 						{digitsArray.map((digit, index) => (
 							<div key={index} className="col">
-								{digit}
+								<div className="digitContainer">{digit}</div>
 							</div>
 						))}
 
